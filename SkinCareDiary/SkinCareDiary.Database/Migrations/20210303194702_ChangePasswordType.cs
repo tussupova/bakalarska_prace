035c237fcda58ecaeff7d43d979892bc467fc89d
Kymbat <xtussupo@mendelu.cz>
2021-03-03 21:53:15 +0100

    Pridani TOKENU a CORS (Jwt). vytvoreni ILoginHelper.cs. zmena passwordu na byte

---
+++ SkinCareDiary/SkinCareDiary.Database/Migrations/20210303194702_ChangePasswordType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SkinCareDiary.Database.Migrations
+{
+    public partial class ChangePasswordType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Password",
+                table: "Users",
+                type: "varbinary(4000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Salt",
+                table: "Users",
+                type: "varbinary(4000)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(4000)",
+                oldNullable: true);
+        }
+    }
+}
