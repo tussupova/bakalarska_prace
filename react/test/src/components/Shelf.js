b98c3ec892e03e40044cdae288a10551b8ffec38
Kymbat <xtussupo@mendelu.cz>
2021-03-26 20:02:15 +0100

    pridavani productu do Shelf.

--- react/test/src/components/Shelf.js
+++ react/test/src/components/Shelf.js
@@ -16,7 +16,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Avatar from "@material-ui/core/Avatar";
 import ListItemText from "@material-ui/core/ListItemText";
 import {downloadPhotosInfoAsync} from "../services/PhotoServices";
-import {getUsersProducts, removeUsersProduct, searchProducts} from "../services/ShelfServices";
+import {addToShelf, getUsersProducts, removeUsersProduct, searchProducts} from "../services/ShelfServices";
 // ISO 3166-1 alpha-2
 // ⚠️ No support for IE 11
 
@@ -47,6 +47,11 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(4),
     marginRight: theme.spacing(4),
     maxWidth: theme.spacing(25)
+  },
+  optionIcon: {
+    maxWidth: theme.spacing(10),
+    maxHeight: theme.spacing(10),
+    margin: theme.spacing(1)
   }
 
 }));
@@ -85,9 +90,9 @@ export default function Shelf() {
       console.log('zacatek hledani', chars)
       const foundedProducts = await searchProducts(inputValue);
       console.log('vysledek', foundedProducts.data);
-      const pr=foundedProducts.data.map((e) => {
+      const pr = foundedProducts.data.map((e) => {
         return {
-          name: e.brand + ' '+e.name,
+          name: e.brand + ' ' + e.name,
           img: e.img,
           id: e.id
         }
@@ -98,103 +103,112 @@ export default function Shelf() {
       console.log(e, "Can not search product");
     }
   }
-  const getX=()=>{
-    console.log("ahoj")
+  const addProduct = async () => {
+    console.log('onChange', selectedProduct.id);
+    try {
+      const res = await addToShelf({
+       productId: selectedProduct.id,
+        routineId: 5,
+      })
+    } catch (e) {
+    console.log(e, "Can not add product to Shelf")
+    }
   }
 
-useEffect(() => {
-  // vola se vzdycky pri renderovani a pouze jednou
-  getProducts();
-}, []);
-const classes = useStyles();
-const [dense, setDense] = React.useState(false);
-const [open, setOpen] = React.useState(false);
-const [options, setOptions] = React.useState([]);
-const [inputValue, setInputValue] = React.useState('');
-const loading = open && options.length === 0;
+  useEffect(() => {
+    // vola se vzdycky pri renderovani a pouze jednou
+    getProducts();
+  }, []);
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const [inputValue, setInputValue] = React.useState('');
+  const loading = open && options.length === 0;
+  const [selectedProduct, setSelectedProduct]=React.useState('');
 
-return (
-  <Grid container>
-    <Grid item xs={12} lg={6} className={classes.searchItem}>
-      <Autocomplete
-        {...options}
+  return (
+    <Grid container>
+      <Grid item xs={12} lg={6} className={classes.searchItem}>
+        <Autocomplete
+          {...options}
 
-        getOptionLabel={(options) => options.name}
-        id="country-select-demo"
-        options={options}
-        classes={{
-          option: classes.option,
-        }}
-        onChange={() => getX()}
-        onInputChange={(event, newInputValue) => {
-          getSearchingProducts(newInputValue)
-        }}
-        autoHighlight
-        renderOption={(options) => (
-          <React.Fragment>
-            {options.id} ({options.name})
-          </React.Fragment>
-        )}
-        renderInput={params => (
-          <TextField
-            {...params}
-            label="Find products"
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            inputProps={{
-              ...params.inputProps,
-              // disable autocomplete and autofill
-            }}
-          />
-        )}
-        /*          renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Choose a country"
-                      variant="outlined"
-                      inputProps={{
-                        ...params.inputProps,
-                        autoComplete: 'new-password', // disable autocomplete and autofill
-                      }}
-                    />
-                  )}*/
-      />
+          getOptionLabel={(options) => options.name}
+          id="country-select-demo"
+          options={options}
+          classes={{
+            option: classes.option,
+          }}
+          onChange={(event, value) => setSelectedProduct(value)}
+          onInputChange={(event, newInputValue) => {
+            getSearchingProducts(newInputValue)
+          }}
+          autoHighlight
+          renderOption={(options) => (
+            <React.Fragment>
+              {<img className={classes.optionIcon} src={options.img}/>} {options.name}
+            </React.Fragment>
+          )}
+          renderInput={params => (
+            <TextField
+              {...params}
+              label="Find products"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              inputProps={{
+                ...params.inputProps,
+                // disable autocomplete and autofill
+              }}
+            />
+          )}
+          /*          renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label="Choose a country"
+                        variant="outlined"
+                        inputProps={{
+                          ...params.inputProps,
+                          autoComplete: 'new-password', // disable autocomplete and autofill
+                        }}
+                      />
+                    )}*/
+        />
+      </Grid>
+      <Grid item className={classes.addItem}>
+        <Button variant="contained" color="primary" onClick={()=>addProduct()}>
+          <Typography>Add Product</Typography>
+        </Button>
+      </Grid>
+      <Grid item xs={12} md={6} className={classes.productItem}>
+        <Typography variant="h6" className={classes.title}>
+          Avatar with text and icon
+        </Typography>
+        <div className={classes.listClass}>
+          <List dense={dense}>
+            {products.map((list) => {
+              return (
+                <ListItem>
+                  <ListItemAvatar>
+                    <img className={classes.productIcon} src={list.img}/>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={list.name}
+                    secondary={list.brand}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon onClick={() => deleteProduct(list.id)}/>
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              )
+            })}
+          </List>
+        </div>
+      </Grid>
     </Grid>
-    <Grid item className={classes.addItem}>
-      <Button variant="contained" color="primary">
-        <Typography>Add Product</Typography>
-      </Button>
-    </Grid>
-    <Grid item xs={12} md={6} className={classes.productItem}>
-      <Typography variant="h6" className={classes.title}>
-        Avatar with text and icon
-      </Typography>
-      <div className={classes.listClass}>
-        <List dense={dense}>
-          {products.map((list) => {
-            return (
-              <ListItem>
-                <ListItemAvatar>
-                  <img className={classes.productIcon} src={list.img}/>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={list.name}
-                  secondary={list.brand}
-                />
-                <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete">
-                    <DeleteIcon onClick={() => deleteProduct(list.id)}/>
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            )
-          })}
-        </List>
-      </div>
-    </Grid>
-  </Grid>
-);
+  );
 }
 
 // From https://bitbucket.org/atlassian/atlaskit-mk-2/raw/4ad0e56649c3e6c973e226b7efaeb28cb240ccb0/packages/core/select/src/data/countries.js
